dc59b3a478a9557dc3b5cdf642f2156c1b2edb22
RISHABH MISHRA <r72782067@gmail.com>
2026-02-20 12:03:21 +0530

    Update CWorkshop.jsx

--- src/pages/CWorkshop.jsx
+++ src/pages/CWorkshop.jsx
@@ -50,7 +50,7 @@ const CWorkshop = () => {
       <Header />
 
       {/* Automatic spacing below fixed header */}
-      <div className="pt-24 sm:pt-28">
+      <div className="pt-24 sm:pt:50">
 
         {/* HERO */}
         <section className="bg-gradient-to-b from-black to-[#120018] text-white px-4 sm:px-6 pb-14 text-center">
